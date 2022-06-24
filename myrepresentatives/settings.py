ef96e1bd2bca7fb9e156167f4a3865bd5dcb48b7
Bryan Moore <31421559+bryanleemoore@users.noreply.github.com>
2022-06-24 13:20:22 -0700

    edit settings

--- myrepresentatives/settings.py
+++ myrepresentatives/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'myrepresentatives',
     'backend.apps.BackendConfig',
+    'whitenoise.runserver_nostatic'
 ]
 
 MIDDLEWARE = [
@@ -53,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'myrepresentatives.urls'
