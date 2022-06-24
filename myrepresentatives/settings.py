eec915d95e292b61a411bf119c3f01c2fa3a7cca
Bryan Moore <31421559+bryanleemoore@users.noreply.github.com>
2022-06-24 13:41:13 -0700

    add public

--- myrepresentatives/settings.py
+++ myrepresentatives/settings.py
@@ -136,6 +136,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'build','staticfiles')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'build/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'public/build/static')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
