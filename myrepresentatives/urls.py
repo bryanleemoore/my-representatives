866a51943bbc72152af09d07e3ca9f4d5fecc36b
Bryan Moore <31421559+bryanleemoore@users.noreply.github.com>
2022-06-24 19:00:17 -0700

    import

--- myrepresentatives/urls.py
+++ myrepresentatives/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
-from ..views import index
+from views import index
 urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
