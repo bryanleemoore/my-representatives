a76dce1592679afa03f38b5c1712d7c3c4127b7e
Bryan Moore <31421559+bryanleemoore@users.noreply.github.com>
2022-06-24 19:35:55 -0700

    te

--- backend/urls.py
+++ backend/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import Representatives, MemberID, Votes, Information
 
 urlpatterns = [
-    path('representatives/', Representatives.as_view()),
-    path('memberID/', MemberID.as_view()),
-    path('votes/', Votes.as_view()),
-    path('info/', Information.as_view())
+    path('representatives', Representatives.as_view()),
+    path('memberID', MemberID.as_view()),
+    path('votes', Votes.as_view()),
+    path('info', Information.as_view())
     
 ]
